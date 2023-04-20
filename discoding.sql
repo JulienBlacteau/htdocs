4c19824746a411b44b2a82f123c5b86c68b3319a
JulienBlacteau <julien.blacteau@edu.esiee-it.fr>
2023-04-20 17:17:49 +0200

    commit db

--- discoding.sql
+++ discoding.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: db
--- Generation Time: May 25, 2021 at 10:24 AM
--- Server version: 8.0.25
--- PHP Version: 7.4.16
+-- Hôte : 127.0.0.1
+-- Généré le : jeu. 20 avr. 2023 à 17:16
+-- Version du serveur : 10.4.27-MariaDB
+-- Version de PHP : 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,24 +18,37 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `discoding`
+-- Base de données : `discoding`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `conversations`
+-- Structure de la table `channel`
+--
+
+CREATE TABLE `channel` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `date` date NOT NULL,
+  `id_server` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `conversations`
 --
 
 CREATE TABLE `conversations` (
-  `id` int NOT NULL,
-  `user1_id` int NOT NULL,
-  `user2_id` int NOT NULL,
+  `id` int(11) NOT NULL,
+  `user1_id` int(11) NOT NULL,
+  `user2_id` int(11) NOT NULL,
   `updated_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Dumping data for table `conversations`
+-- Déchargement des données de la table `conversations`
 --
 
 INSERT INTO `conversations` (`id`, `user1_id`, `user2_id`, `updated_at`) VALUES
@@ -45,17 +58,17 @@ INSERT INTO `conversations` (`id`, `user1_id`, `user2_id`, `updated_at`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `friends`
+-- Structure de la table `friends`
 --
 
 CREATE TABLE `friends` (
-  `id` int NOT NULL,
-  `user_id` int NOT NULL,
-  `friend_user_id` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `friend_user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Dumping data for table `friends`
+-- Déchargement des données de la table `friends`
 --
 
 INSERT INTO `friends` (`id`, `user_id`, `friend_user_id`) VALUES
@@ -65,19 +78,19 @@ INSERT INTO `friends` (`id`, `user_id`, `friend_user_id`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `messages`
+-- Structure de la table `messages`
 --
 
 CREATE TABLE `messages` (
-  `id` int NOT NULL,
-  `conversation_id` int NOT NULL,
-  `user_id` int NOT NULL,
+  `id` int(11) NOT NULL,
+  `conversation_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `content` text NOT NULL,
   `created_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Dumping data for table `messages`
+-- Déchargement des données de la table `messages`
 --
 
 INSERT INTO `messages` (`id`, `conversation_id`, `user_id`, `content`, `created_at`) VALUES
@@ -87,32 +100,68 @@ INSERT INTO `messages` (`id`, `conversation_id`, `user_id`, `content`, `created_
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Structure de la table `message_channel`
+--
+
+CREATE TABLE `message_channel` (
+  `id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `server`
+--
+
+CREATE TABLE `server` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `icon_url` text NOT NULL,
+  `date` date NOT NULL,
+  `id_users` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
 --
 
 CREATE TABLE `users` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
   `email` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `avatar_url` varchar(500) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Dumping data for table `users`
+-- Déchargement des données de la table `users`
 --
 
 INSERT INTO `users` (`id`, `email`, `username`, `password`, `avatar_url`) VALUES
 (1, 'coding@factory.fr', 'coding', '123456', 'https://media-exp1.licdn.com/dms/image/C560BAQFveTMznUt80w/company-logo_200_200/0/1606411224030?e=2159024400&v=beta&t=Q_n0Ieldw9WSqZs5sNwqS4cfTKRJW1nmud2xhjRrgZM'),
 (2, 'robin@factory.fr', 'robin', 'robin', 'https://i.pinimg.com/originals/92/57/8a/92578adbf3632f085bffdc00c0eccb47.jpg'),
-(3, 'bob@bob.com', 'bob', 'bob', NULL);
+(3, 'bob@bob.com', 'bob', 'bob', NULL),
+(4, 'julien@coding.com', 'julien#cf18', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', NULL),
+(5, 'julien@coding.fr', 'julien2#cfa6', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', NULL),
+(6, 'julien3@blabla.fr', 'julien3#c96f', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', NULL);
 
 --
--- Indexes for dumped tables
+-- Index pour les tables déchargées
 --
 
 --
--- Indexes for table `conversations`
+-- Index pour la table `channel`
+--
+ALTER TABLE `channel`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `conversations`
 --
 ALTER TABLE `conversations`
   ADD PRIMARY KEY (`id`),
@@ -120,7 +169,7 @@ ALTER TABLE `conversations`
   ADD KEY `fk_conversations_to_user2_id` (`user2_id`);
 
 --
--- Indexes for table `friends`
+-- Index pour la table `friends`
 --
 ALTER TABLE `friends`
   ADD PRIMARY KEY (`id`),
@@ -128,7 +177,7 @@ ALTER TABLE `friends`
   ADD KEY `fk_friends_to_friend_user_id` (`friend_user_id`);
 
 --
--- Indexes for table `messages`
+-- Index pour la table `messages`
 --
 ALTER TABLE `messages`
   ADD PRIMARY KEY (`id`),
@@ -136,59 +185,89 @@ ALTER TABLE `messages`
   ADD KEY `fk_messages_to_user_id` (`user_id`);
 
 --
--- Indexes for table `users`
+-- Index pour la table `message_channel`
+--
+ALTER TABLE `message_channel`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `server`
+--
+ALTER TABLE `server`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT for table `conversations`
+-- AUTO_INCREMENT pour la table `channel`
+--
+ALTER TABLE `channel`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `conversations`
 --
 ALTER TABLE `conversations`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `friends`
+-- AUTO_INCREMENT pour la table `friends`
 --
 ALTER TABLE `friends`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `messages`
+-- AUTO_INCREMENT pour la table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT pour la table `message_channel`
+--
+ALTER TABLE `message_channel`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `server`
+--
+ALTER TABLE `server`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- Constraints for dumped tables
+-- Contraintes pour les tables déchargées
 --
 
 --
--- Constraints for table `conversations`
+-- Contraintes pour la table `conversations`
 --
 ALTER TABLE `conversations`
   ADD CONSTRAINT `fk_to_user1_id` FOREIGN KEY (`user1_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `fk_to_user2_id` FOREIGN KEY (`user2_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `friends`
+-- Contraintes pour la table `friends`
 --
 ALTER TABLE `friends`
   ADD CONSTRAINT `fk_to_friend_user_id` FOREIGN KEY (`friend_user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `fk_to_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `messages`
+-- Contraintes pour la table `messages`
 --
 ALTER TABLE `messages`
   ADD CONSTRAINT `fk_messages_to_conversation_id` FOREIGN KEY (`conversation_id`) REFERENCES `conversations` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
